c638cf8cdf35a0322b0adb5282845bcd9b9fd013
Shawn Cicoria <github@cicoria.com>
2020-11-06 22:25:07 -0500

    updates to settings

--- hotelapp/reservations/apps.py
+++ hotelapp/reservations/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
-
-
+# https://stackoverflow.com/a/32795405/140618
+# https://learndjango.com/tutorials/django-signup-tutorial
 class ReservationsConfig(AppConfig):
     name = 'reservations'
