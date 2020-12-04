9086f01f2e896be6a587f00fb1f494483ab83e71
Shawn Cicoria <github@cicoria.com>
2020-12-04 11:55:49 -0500

    add user script

---
+++ data/000-users.sql
@@ -0,0 +1,4 @@
+INSERT INTO customer (password,last_login,is_superuser,first_name,last_name,is_staff,is_active,date_joined,email,friendly_name,address,phone) VALUES
+	 ('pbkdf2_sha256$216000$6Mcb5VRgw4wH$AEs25gTeVj7npMCFZqv+cjNDvMhgvRgvuRecUwJ+E0Q=','2020-12-04 13:52:49.669387',0,'','',0,1,'2020-12-04 13:52:05.884373','one@home.com','NA','NA','NA'),
+	 ('pbkdf2_sha256$216000$rxlF0MmavtMD$8XWuPDELu/nzCbhjXCAtZxdmX6PZn4BYOCD7PKPqkSs=','2020-12-04 13:52:38.844729',0,'','',0,1,'2020-12-04 13:52:26.721007','two@home.com','NA','NA','NA');
+   
